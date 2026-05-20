b732040fe593dd321d6e14585a8d35df12f0a107
tahamukhtar20 <tahamukhtar20@gmail.com>
2026-05-20 22:22:59 +0500

    chore: initialise repo

---
+++ commitlint.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('@commitlint/types').UserConfig} */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [0],
+    'footer-max-line-length': [0],
+  },
+};
