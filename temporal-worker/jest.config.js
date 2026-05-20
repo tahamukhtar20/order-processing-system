21b0a7457fd304e5ae9ed3997a40b263396c2dd6
tahamukhtar20 <tahamukhtar20@gmail.com>
2026-05-21 00:49:43 +0500

    fix(worker-base): replace jest.config.ts with jest.config.js to avoid ts-node dependency

--- temporal-worker/jest.config.js
+++ temporal-worker/jest.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
@@ -9,4 +8,4 @@ const config: Config = {
   },
 };
 
-export default config;
+module.exports = config;
